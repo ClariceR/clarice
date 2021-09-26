793f96a8474a6c733915acb5d80cc5e4b1cb71a1
clarice_r <claricerdallorto@gmail.com>
2021-09-26 13:38:46 +0100

    All links working

--- app/js/script.js
+++ app/js/script.js
@@ -4,7 +4,7 @@ const projects = document.querySelector("#projects-link");
 const about = document.querySelector("#about-link");
 const contact = document.querySelector("#contact-link");
 const github = document.querySelector("#github-link");
-// const overlay = document.querySelector(".overlay-nav-menu-mobile");
+// const overlay = document.querySelector(".overlay-menu-mobile");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("menu-toggle");
@@ -47,10 +47,9 @@ github.addEventListener("click", () => {
 });
 
 // overlay.addEventListener("click", () => {
-//   if (menuBtn.classList.contains("menu-toggle")) {
-//     menuBtn.classList.remove("menu-toggle");
-//     mobileMenu.classList.remove("anim-fade-in");
-//     mobileMenu.classList.remove("open");
-//     mobileMenu.classList.add("anim-fade-out");
-//   }
+//   menuBtn.classList.toggle("menu-toggle");
+
+//   !menuBtn.classList.contains("menu-toggle")
+//     ? mobileMenu.classList.remove("open")
+//     : mobileMenu.classList.add("open");
 // });
